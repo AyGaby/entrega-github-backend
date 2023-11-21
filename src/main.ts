03ac6b49aece09c84bda0f4ff13a5f340453f646
AyGaby <gbb888021@gmail.com>
2023-11-21 18:36:40 -0300

    en busca del mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import app from "./server";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/tu-base-de-datos", {});
+const db = mongoose.connection;
+
+db.on(
+  "error",
+  console.error.bind(console, "Error de conexión a la base de datos:")
+);
+db.once("open", function () {
+  console.log("Conexión exitosa a la base de datos");
+});
 
 //levantamos el servidor en el puerto especificado
 app.listen(4000),
